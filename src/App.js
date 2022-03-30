7db527e21c4b354fb342659ebd70c8ca4e09f747
Varma <anjivarma.penmatsa@gmail.com>
2022-03-30 15:41:15 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux';
+import { depositMoney, withdrawMoney} from './State/action/action';
+import { bindActionCreators } from 'redux'
 
 function App() {
+
+  const account = useSelector(state => state.account)
+  const dispatch = useDispatch();
+  const [amount, setamount] = useState()
+  const action = bindActionCreators({depositMoney, withdrawMoney}, dispatch)
+
+  console.log(account)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{account}</h1>
+      <div>
+        <input
+            type="text" value={amount} placeholder="Enter the Money"
+            onChange={(e)=>{
+              e.preventDefault();
+              setamount(e.target.value)
+            }}
+        />
+      </div>
+      <br/>
+      <div>
+        <button onClick={()=> action.depositMoney(Number(amount))}>Deposit</button>
+        <button onClick={() => action.withdrawMoney(Number(amount))}>Withdraw</button>
+      </div>
     </div>
   );
 }
